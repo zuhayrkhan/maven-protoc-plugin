89049c9584119d36b81d3d6ce0e54d68cd575ef2
Zuhayr Khan <zuhayrkhan@steinway.(none)>
2010-04-21 18:38:15 +0100

    re-worked protocPlugin parameter to contain name and path elements to reduce possible errors in maven config

---
+++ src/main/java/com/google/protobuf/maven/ProtocPlugin.java
@@ -0,0 +1,11 @@
+package com.google.protobuf.maven;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: zuhayrkhan
+ * Date: 21-Apr-2010
+ * Time: 07:21:26
+ * To change this template use File | Settings | File Templates.
+ */
+public class ProtocPlugin {
+}
